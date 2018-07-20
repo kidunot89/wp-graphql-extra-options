0ad4d5320fa1f5242f2b31582b37bd4612c868d5
Geoff Taylor <geoffrey.taylor@outlook.com>
2018-07-19 23:55:05 -0400

    error fixed in admin/class-wp-graphql-extra-options-admin.php

--- includes/class-wp-graphql-extra-options.php
+++ includes/class-wp-graphql-extra-options.php
@@ -100,6 +100,11 @@ class Wp_Graphql_Extra_Options {
 	private function load_dependencies() {
 
 		/**
+		 * Load WPGraphQL if not loaded
+		 */
+		include_once( PLUGINDIR . '/wp-graphql/vendor/autoload.php' );
+
+		/**
 		 * The class responsible for orchestrating the actions and filters of the
 		 * core plugin.
 		 */
